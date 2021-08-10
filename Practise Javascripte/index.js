605fa8831ad6e2cc23f63d3684db40e74b618673
Abir masum <87767663+Abirmasum@users.noreply.github.com>
2021-08-10 23:57:30 +0600

    Add files via upload

---
+++ Practise Javascripte/index.js
@@ -0,0 +1,75 @@
+console.log("Hellow Abir");
+console.log("This line is for print js in console");
+console.log("Hellow Harry");
+console.log("");
+document.getElementsByClassName('container');
+document.write("My name is Abir masum And I am a student");
+console.warn("This is a warning");
+console.error("This is an error");
+console.assert(4==4);
+document.write("This is ami ar kisu valo kore likte parina");
+document.write("this is a new line");
+console.warn("please write your massege carefully");
+console.error(" this massege show when you get an error");
+// alert("Invalid password");
+if(4==5){
+    alert("The number is valid");
+}
+var num1 = 10;
+var num2 = 20;
+console.log(num1+num2);
+console.log(num2-num1);
+console.log(num2/num1);
+console.log(num2*num1);
+var marks={
+    Abir: 10,
+    sabbir: 13,
+    Tanbir: 30,
+}
+console.log(marks);
+var a = true;
+var b = false;
+var und
+var n= null;
+console.log(n);
+console.log(a);
+console.log(b);
+
+//  Array and collecton
+// There is two type of data type in Array
+// 1. Primitive Data type
+//  There are 6 kind of primitive Data type
+// 1. Number 2.Null 3.string 4.boolen 5.symbole
+var arr = [1,2,3,4,5,6]
+console.log(arr[0]);
+console.log(arr[4]);
+console.log(arr[3]);
+// 2. referance Data type
+console.log( "This is 2+2=",arr[3+0]);
+// Arthmetic  Operator 
+var num3=30;
+var num4=40;
+console.log("This is the value of 30+40=",num3+num4);
+console.log("This value of 40-30 is =",num4-num3);
+// assignment Operators
+var c=num4;
+console.log(c);
+console.log(c+=2);
+console.log(c-=10);
+// Comparition Operators
+var x=24;
+var y=20;
+console.log(x==y);
+console.log(x<=y);
+console.log(x>=y);
+console.log(x+y);
+// LOgical operators AND
+console.log(true && true);
+console.log(true && false);
+// Logical operator OR
+console.log(true || false);
+console.log(false || false);
+// Logical operator Not
+console.log(!true);
+console.log(!false);
+
